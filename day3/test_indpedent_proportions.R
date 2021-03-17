05f5c10a5815a27ec17870230ed2e27882933e34
keniajin <keniajin@gmail.com>
2021-03-17 11:48:11 +0300

    added prop test practical

--- day3/test_indpedent_proportions.R
+++ day3/test_indpedent_proportions.R
@@ -1,6 +1,5 @@
-
 library(tidyverse)
-
+library(freqtables)
 ## read in the data
 bw_data <- read.csv("birthweight2.csv")
 
@@ -13,4 +12,14 @@ prop.table(tbl_sex , margin = 1)
 prop.table(tbl_sex , margin = 2)
 
 ## test of proportion
+## we observer no significant difference (pvalue)
 prop.test(tbl_sex)
+
+## the confidence intervals overlap
+bw_data %>% 
+  ## create the frequency table
+  freq_table(sex, lbw2) %>%
+  ## filter only low birthweights
+  filter(col_cat==0) %>%
+  ## select only variables containing row
+  select(contains('row'))
